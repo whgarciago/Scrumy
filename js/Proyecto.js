5653cf0c7f0d623356817ac20e91555020509e91
josanchezva <josanchezva@unal.edu.co>
2021-11-29 18:10:29 -0500

    actualización vista principal

---
+++ js/Proyecto.js
@@ -0,0 +1,14 @@
+class Proyecto{
+
+    //TODO:get the userid
+    constructor(idProyecto,nombre,usuarioID,motivacion,metaID,descripcion,fechaFinalizacion){
+        this.idProyecto=idProyecto;
+        this.nombre=nombre;
+        this.usuarioID=1;
+        this.motivacion=motivacion;
+        this.metaID=metaID;
+        this.descripcion=descripcion;
+        this.fechaFinalizacion=fechaFinalizacion;
+    }
+    
+}
